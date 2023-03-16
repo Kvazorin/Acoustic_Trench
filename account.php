210777c804395dba5663a105815af9104b2fb9e0
Kvazorin <povaroww@gmail.com>
2023-03-16 21:16:20 +0300

    Second commit
    - Added stylings for "account.php"

--- account.php
+++ account.php
@@ -71,13 +71,41 @@ require_once __DIR__ . '/inc/funcs.php';
         <main>
             <div class="wrapper mt-5">
                 <div class="container">
-                    <h1 class="mt-5">Управление аккаунтом</h1>
-                    <h3 class="mt-4">
-                        <?php echo strval($_SESSION['username']) ?>
-                    </h3>
 
-                    <a style="font-size: 16px;" href="inc/logout.php">Выйти</a>
-                    <p style="padding-bottom: 200px;" class="mt-5">Настройки. Тудам сюдам</p>
+                    <div class="flex__container">
+                        <div class="flex__item-1">
+                            <h1>Управление аккаунтом</h1>
+                            <h3 class="mt-4 user">
+                                <?php echo strval($_SESSION['username']) ?>
+                                <a class="logout_link link" style="font-size: 16px;" href="inc/logout.php">Выйти</a>
+                            </h3>
+
+                            <table class="table" style="width: 472px;">
+                                <thead>
+                                    <tr>
+                                        <th scope="col" style="padding-left: 0;">Платежные данные</th>
+                                        <th scope="col">Email</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td style="padding-left: 0;">Mastercard 64***<i class="fas fa-pen" style="font-size: 14px; margin-left: 5px;"></i></td>
+                                        <td>example@email.ru<i class="fas fa-pen" style="font-size: 14px; margin-left: 5px;"></i></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+
+                            <h3 class="article user_info__article">Количество совершенных заказов: 25</h3>
+                            <h3 class="article user_info__article mb-5">На сумму: 239 000 руб.</h3>
+                            <h3 style="font-size: 16px;">Персональные предложения</h3>
+                            <button class="btn personal_offers__btn">Посмотреть</button>
+                        </div>
+                        <div class="flex__item-2">
+                            <h1>Фото профиля</h1>
+                            <img src="img/profile_photo.png" alt="profile_photo" class="mb-4" style="max-width: 216px;">
+                            <a href="#" class="link">Изменить фото профиля<i class="fas fa-pen" style="font-size: 14px; margin-left: 5px;"></i></a>
+                        </div>
+                    </div>
 
                 </div><!-- /container -->
             </div><!-- /wrapper -->
